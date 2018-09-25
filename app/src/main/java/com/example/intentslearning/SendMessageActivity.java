4a6e777c8441e0f2123382d9e9d79dbf4ae38862
Saul <saulftopete2@gmail.com>
2018-09-25 09:35:18 -0700

    New stuff

--- app/src/main/java/com/example/intentslearning/SendMessageActivity.java
+++ app/src/main/java/com/example/intentslearning/SendMessageActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class SendMessageActivity extends AppCompatActivity {
-    
+
     private Button sendMessage;
-    private EditText editTestMessage;
+    private Button shareButton;
+    private EditText editTextMessage;
 
     public static final String EXTRA_SENT_MESSAGE = "the message";
 
@@ -26,21 +26,37 @@ public class SendMessageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //get text from edit text
-                String message = editTestMessage.getText().toString();
+                String message = editTextMessage.getText().toString();
                 //create an intent
                 //the arguments are where you are coming from (this)
                 //and where you are going (TargetActivity.class)
                 Intent intentSendMessage = new Intent(SendMessageActivity.this, RecieveMessageActivity.class);
                 //package the text into the intent
-                intentSendMessage.putExtra("themessage", message);
+                intentSendMessage.putExtra(EXTRA_SENT_MESSAGE, message);
                 //start the activity
                 startActivity(intentSendMessage);
+            }
+        });
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //make an intent with the desired action
+               Intent intentShare = new Intent(Intent.ACTION_SEND);
+               //set the data type of the stuff we're packaging away
+                //can look up that type you need on the internet
+                intentShare.setType("text/plain");
+                //put the extra with the message
+                intentShare.putExtra(EXTRA_SENT_MESSAGE, editTextMessage.getText().toString());
+                //launch the activity
+                startActivity(intentShare);
             }
         });
     }
 
     private void wireWidgets() {
         sendMessage.findViewById(R.id.button_sendmessage_send);
-        editTestMessage.findViewById(R.id.edittext_sendmessage_send);
+        editTextMessage.findViewById(R.id.edittext_sendmessage_send);
+        shareButton.findViewById(R.id.button_sendmessage_share);
     }
 }
